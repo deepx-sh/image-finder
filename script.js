a7f1c5e79845af52ca6bccb2944b1bb5fd5c56a7
Deep Prajapati <dvprajapati7212@gmail.com>
2025-09-18 18:29:33 +0530

    feat: add validation and event listener on search buttion

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const form = document.querySelector(".search-form");
+const form = document.querySelector("#search-form");
+const inp = document.querySelector("#search-box");
+const searchBtn = document.querySelector(".search-btn");
+const ic=document.querySelector(".image-container")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+});
+
+searchBtn.addEventListener("click", () => {
+    let keyword = inp.ariaValueMax;
+
+    if (keyword.trim() === '' || keyword.trim().length === 0) {
+        alert("Please enter a search keyword.");
+    }
+    else {
+        getImages(keyword);
+    }
 })
