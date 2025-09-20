cc70cd06cdd6dd34d3c5e859dd12b2c34ed53e16
Deep Prajapati <dvprajapati7212@gmail.com>
2025-09-20 20:27:48 +0530

    feat: implement error and loading function

--- script.js
+++ script.js
@@ -3,46 +3,60 @@ const inp = document.querySelector("#search-box");
 const searchBtn = document.querySelector(".search-btn");
 const ic=document.querySelector(".image-container")
 const showMoreBtn=document.querySelector("#show-more-btn")
-
+const errorMessage = document.querySelector("#error-message")
+const loading = document.querySelector("#loading");
 let page = 1;
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    handleSearch();
+});
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    handleSearch()
+});
+
+
+function handleSearch() {
+     let keyword = inp.value.trim();
+
+    if (!keyword) {
+        showError("Please enter a search term to discover amazing images");
+        return;
+    }
+
     page = 1;
-});
-
-searchBtn.addEventListener("click", () => {
-    let keyword = inp.value;
-
-    if (keyword.trim() === '' || keyword.trim().length === 0) {
-        alert("Please enter a search keyword.");
-        inp.value = '';
-    }
-    else {
-        ic.innerHTML=""
-        getImages(keyword);
-    }
-});
-
-
+    ic.innerHTML = "";
+    hideError();
+    getImages(keyword)
+}
 
 async function getImages(keyword) {
-    
+    showLoading();
     try {
         let url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=&per_page=12`;
         
         const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`API Error: ${response.status}`);
+        }
         const result = await response.json();
 
         const data = result.results;
+        if (data.length ===0 && page === 1) {
+            showError("No image found. Try different search term");
+            hideLoading();
+            return;
+        }
         showImages(data);
-        console.log(data);
+
         
         
     } catch (error) {
-        console.log(error);
-        
+        showError("Oops! Something went wrong. Please try again");
     }
+    hideLoading();
 }
 
 function showImages(data) {
@@ -65,4 +79,21 @@ function showImages(data) {
 showMoreBtn.addEventListener("click", () => {
     page++;
     getImages(inp.value);
-})
+});
+
+function showError(message) {
+    errorMessage.textContent = message;
+    errorMessage.style.display = "block";
+}
+
+function hideError() {
+    errorMessage.style.display="none"
+}
+
+function showLoading() {
+    loading.style.display = "block";
+}
+
+function hideLoading() {
+    loading.style.display="none"
+}
